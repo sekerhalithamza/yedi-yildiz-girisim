c9875af42005935cc89487673ded13057d6e61a9
Halit Hamza Şeker <shalithamza@gmail.com>
2023-06-21 12:50:39 +0300

    fix: vcard

--- src/js/script.js
+++ src/js/script.js
@@ -2,52 +2,53 @@
 
 // Vcard function
 
-const linkArray = document.getElementsByClassName("vcard__content");
+if (window.location.href.endsWith("hasanyediyildiz.html")) {
+  const linkArray = document.getElementsByClassName("vcard__content");
 
-for (const links of linkArray) {
-  links.addEventListener("click", function () {
-    links.classList.toggle("open");
+  for (const links of linkArray) {
+    links.addEventListener("click", function () {
+      links.classList.toggle("open");
+    });
+  }
+
+  const vcardDownloadBtn = document.getElementById("vcardDownloadBtn");
+
+  vcardDownloadBtn.addEventListener("click", function () {
+    var userName;
+    var companyPos;
+    var email;
+    var tel;
+    if (window.location.href.endsWith("hasanyediyildiz.html")) {
+      userName = "Hasan Yediyıldız";
+      companyPos = "Founder";
+      email = "hyediyildiz@gmail.com";
+      tel = "+905326517788";
+    }
+    var vcardContent =
+      "BEGIN:VCARD\n" +
+      "VERSION:3.0\n" +
+      "FN:" +
+      userName +
+      "\n" +
+      "ORG:Yediyıldız Girişim Ofisi\n" +
+      "TITLE:" +
+      companyPos +
+      "\n" +
+      "EMAIL:" +
+      email +
+      "\n" +
+      "TEL;TYPE=cell,voice:" +
+      tel +
+      "\n" +
+      "ADR;TYPE=work:;; Konaklar Mahallesi; İstanbul; Beşiktaş; 34330; Türkiye\n" +
+      "END:VCARD";
+
+    var fileName = userName + ".vcf";
+    var blob = new Blob([vcardContent], { type: "text/vcard" });
+    var downloadLink = document.createElement("a");
+    downloadLink.href = window.URL.createObjectURL(blob);
+    downloadLink.download = fileName;
+    downloadLink.click();
+    URL.revokeObjectURL(downloadLink.href);
   });
 }
-
-const vcardDownloadBtn = document.getElementById("vcardDownloadBtn");
-
-vcardDownloadBtn.addEventListener("click", function () {
-  console.log("clicked");
-  var userName;
-  var companyPos;
-  var email;
-  var tel;
-  if (window.location.href.endsWith("hasanyediyildiz.html")) {
-    userName = "Hasan Yediyıldız";
-    companyPos = "Founder";
-    email = "hyediyildiz@gmail.com";
-    tel = "+905326517788";
-  }
-  var vcardContent =
-    "BEGIN:VCARD\n" +
-    "VERSION:3.0\n" +
-    "FN:" +
-    userName +
-    "\n" +
-    "ORG:Yediyıldız Girişim Ofisi\n" +
-    "TITLE:" +
-    companyPos +
-    "\n" +
-    "EMAIL:" +
-    email +
-    "\n" +
-    "TEL;TYPE=cell,voice:" +
-    tel +
-    "\n" +
-    "ADR;TYPE=work:;; Konaklar Mahallesi; İstanbul; Beşiktaş; 34330; Türkiye\n" +
-    "END:VCARD";
-
-  var fileName = userName + ".vcf";
-  var blob = new Blob([vcardContent], { type: "text/vcard" });
-  var downloadLink = document.createElement("a");
-  downloadLink.href = window.URL.createObjectURL(blob);
-  downloadLink.download = fileName;
-  downloadLink.click();
-  URL.revokeObjectURL(downloadLink.href);
-});
