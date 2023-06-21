7524791136dfa9f2ede8afcba61b7ac2569cd7b1
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-06-21 10:29:23 +0300

    a

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const linkArray = document.getElementsByClassName("vcard__content");
-
-for (const links of linkArray) {
-  links.addEventListener("click", function () {
-    links.classList.toggle("open");
-  });
-}
