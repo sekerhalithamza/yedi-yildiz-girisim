540653e0bac4894a0d6d3e696877363e7e0ed6cb
Halit Hamza Şeker <shalithamza@gmail.com>
2023-07-06 10:52:10 +0300

    add: languageBtn and fix: responsive
    @Notenlish

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,21 @@
 
 const hamburgerBtn = document.getElementById("hamburgerBtn");
 const sideBar = document.getElementById("sideBar");
+const languageBtn = document.getElementById("languageBtn");
+const textList = document.getElementsByClassName("text");
 
 hamburgerBtn.addEventListener("click", function () {
   hamburgerBtn.classList.toggle("active");
   sideBar.classList.toggle("active");
+});
+
+languageBtn.addEventListener("click", function () {
+  for (let textElement of textList) {
+    let oldText = textElement.innerText;
+    textElement.innerText = textElement.getAttribute("data-language");
+    textElement.setAttribute("data-language", oldText);
+  }
+  let oldText = languageBtn.innerText;
+  languageBtn.innerText = languageBtn.getAttribute("data-language");
+  languageBtn.setAttribute("data-language", oldText);
 });
