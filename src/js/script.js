65309eab5d0020f12e88976d6e9dcf70739f522e
Halit Hamza Şeker <shalithamza@gmail.com>
2023-07-21 11:40:35 +0300

    add: pop-ups

--- src/js/script.js
+++ src/js/script.js
@@ -7,4 +7,6 @@ languageBtn.addEventListener("click", () => {
     element.innerText = element.getAttribute("data-language");
     element.setAttribute("data-language", oldText);
   }
+  if (document.documentElement.lang === "en") document.documentElement.lang = "tr";
+  else document.documentElement.lang = "en";
 });
