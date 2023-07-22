ab0eaac59515d021e942fd6338fe2bcafe667881
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-07-22 10:58:24 +0300

    add lang changing and fix icon disapperaing

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,30 @@
+let currentLang = localStorage.getItem("lang");
+if (currentLang == null) {
+  currentLang = "EN";
+  localStorage.setItem("lang", currentLang);
+}
+
+console.log("currentlang", currentLang);
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(currentLang);
+  if (currentLang == "EN") {
+  } else if (currentLang == "TR") {
+    applyLang();
+  }
+});
+
 const languageBtn = document.getElementById("languageBtn");
 const textElements = document.getElementsByClassName("text");
 
-languageBtn.addEventListener("click", () => {
+languageBtn.addEventListener("click", applyLang);
+
+function applyLang() {
+  if (currentLang === "EN") {
+    currentLang = "TR";
+  } else if (currentLang === "TR") {
+    currentLang = "EN";
+  }
   for (const element of textElements) {
     let oldText = element.innerText;
     element.innerText = element.getAttribute("data-language");
@@ -9,4 +32,6 @@ languageBtn.addEventListener("click", () => {
   }
   if (document.documentElement.lang === "en") document.documentElement.lang = "tr";
   else document.documentElement.lang = "en";
-});
+  localStorage.setItem("lang", currentLang);
+  console.log(currentLang);
+}
